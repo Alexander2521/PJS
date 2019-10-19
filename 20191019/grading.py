c402897919b991e7f38dd2cd58832e6690bbcdab
Alexander2521 <56187924+Alexander2521@users.noreply.github.com>
2019-10-19 17:59:01 +0900

    3

---
+++ 20191019/grading.py
@@ -0,0 +1,18 @@
+# score = 94.7
+score = input()
+# print(score)
+ # 94.7 "94.7"
+
+
+if(float(score) >= 90):
+    print("A")
+# if(float(score) >= 80 and float(score) < 90 ) :
+#     print("B")
+elif(float(score) >=80) :
+    print("B")
+elif(float(score) >=70) :
+    print("C")
+elif(float(score) >= 60):
+    print("D")
+else:
+    print(" F, you must study harder")
---
+++ 20191019/while.py
@@ -0,0 +1,12 @@
+Tom = 10
+marshEaten = 0
+
+while (marshEaten < Tom):
+    marshEaten = marshEaten + 1
+    if(marshEaten >= Tom ):
+        print("im full")
+    else :
+        print (" I ate " + str(marshEaten) + " marshmellows, I want more!")
+
+# if(marshEaten >= Tom ):
+#     print("im full")
